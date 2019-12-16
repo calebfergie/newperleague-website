a6cfae8752d08cc0d264bc99bcc69d937a3ec2c9
calebfergie <yonohabloespanol@gmail.com>
2019-12-16 11:57:17 -0500

    remapsource files for heroku

--- bs-vendor/jquery/jquery.slim.js
+++ bs-vendor/jquery/jquery.slim.js
@@ -4006,7 +4006,7 @@ function fcamelCase( all, letter ) {
 
 // Convert dashed to camelCase; used by the css and data modules
 // Support: IE <=9 - 11, Edge 12 - 15
-// Microsoft forgot to hump their vendor prefix (#9572)
+// Microsoft forgot to hump their bs-vendor prefix (#9572)
 function camelCase( string ) {
 	return string.replace( rmsPrefix, "ms-" ).replace( rdashAlpha, fcamelCase );
 }
@@ -6410,12 +6410,12 @@ function addGetHookIf( conditionFn, hookFn ) {
 
 var cssPrefixes = [ "Webkit", "Moz", "ms" ],
 	emptyStyle = document.createElement( "div" ).style,
-	vendorProps = {};
+	bs-vendorProps = {};
 
-// Return a vendor-prefixed property or undefined
-function vendorPropName( name ) {
+// Return a bs-vendor-prefixed property or undefined
+function bs-vendorPropName( name ) {
 
-	// Check for vendor prefixed names
+	// Check for bs-vendor prefixed names
 	var capName = name[ 0 ].toUpperCase() + name.slice( 1 ),
 		i = cssPrefixes.length;
 
@@ -6427,9 +6427,9 @@ function vendorPropName( name ) {
 	}
 }
 
-// Return a potentially-mapped jQuery.cssProps or vendor prefixed property
+// Return a potentially-mapped jQuery.cssProps or bs-vendor prefixed property
 function finalPropName( name ) {
-	var final = jQuery.cssProps[ name ] || vendorProps[ name ];
+	var final = jQuery.cssProps[ name ] || bs-vendorProps[ name ];
 
 	if ( final ) {
 		return final;
@@ -6437,7 +6437,7 @@ function finalPropName( name ) {
 	if ( name in emptyStyle ) {
 		return name;
 	}
-	return vendorProps[ name ] = vendorPropName( name ) || name;
+	return bs-vendorProps[ name ] = bs-vendorPropName( name ) || name;
 }
 
 
--- gulpfile.js
+++ gulpfile.js
@@ -42,31 +42,31 @@ function browserSyncReload(done) {
   done();
 }
 
-// Clean vendor
+// Clean bs-vendor
 function clean() {
-  return del(["./vendor/"]);
+  return del(["./bs-vendor/"]);
 }
 
-// Bring third party dependencies from node_modules into vendor directory
+// Bring third party dependencies from node_modules into bs-vendor directory
 function modules() {
   // Bootstrap
   var bootstrap = gulp.src('./node_modules/bootstrap/dist/**/*')
-    .pipe(gulp.dest('./vendor/bootstrap'));
+    .pipe(gulp.dest('./bs-vendor/bootstrap'));
   // Font Awesome CSS
   var fontAwesomeCSS = gulp.src('./node_modules/@fortawesome/fontawesome-free/css/**/*')
-    .pipe(gulp.dest('./vendor/fontawesome-free/css'));
+    .pipe(gulp.dest('./bs-vendor/fontawesome-free/css'));
   // Font Awesome Webfonts
   var fontAwesomeWebfonts = gulp.src('./node_modules/@fortawesome/fontawesome-free/webfonts/**/*')
-    .pipe(gulp.dest('./vendor/fontawesome-free/webfonts'));
+    .pipe(gulp.dest('./bs-vendor/fontawesome-free/webfonts'));
   // jQuery Easing
   var jqueryEasing = gulp.src('./node_modules/jquery.easing/*.js')
-    .pipe(gulp.dest('./vendor/jquery-easing'));
+    .pipe(gulp.dest('./bs-vendor/jquery-easing'));
   // jQuery
   var jquery = gulp.src([
       './node_modules/jquery/dist/*',
       '!./node_modules/jquery/dist/core.js'
     ])
-    .pipe(gulp.dest('./vendor/jquery'));
+    .pipe(gulp.dest('./bs-vendor/jquery'));
   return merge(bootstrap, fontAwesomeCSS, fontAwesomeWebfonts, jquery, jqueryEasing);
 }
 
@@ -123,15 +123,15 @@ function watchFiles() {
 }
 
 // Define complex tasks
-const vendor = gulp.series(clean, modules);
-const build = gulp.series(vendor, gulp.parallel(css, js));
+const bs-vendor = gulp.series(clean, modules);
+const build = gulp.series(bs-vendor, gulp.parallel(css, js));
 const watch = gulp.series(build, gulp.parallel(watchFiles, browserSync));
 
 // Export tasks
 exports.css = css;
 exports.js = js;
 exports.clean = clean;
-exports.vendor = vendor;
+exports.bs-vendor = bs-vendor;
 exports.build = build;
 exports.watch = watch;
 exports.default = build;
